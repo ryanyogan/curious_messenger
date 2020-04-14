e4e9e809185d8140b899360b33ddfdb7a2b562b2
ryanyogan <ryanyogan@gmail.com>
2020-04-13 18:39:01 -0700

    liveview 0.11

--- lib/curious_messenger_web.ex
+++ lib/curious_messenger_web.ex
@@ -23,6 +23,7 @@ defmodule CuriousMessengerWeb do
 
       import Plug.Conn
       import CuriousMessengerWeb.Gettext
+      import Phoenix.LiveView.Controller
       alias CuriousMessengerWeb.Router.Helpers, as: Routes
     end
   end
@@ -41,6 +42,7 @@ defmodule CuriousMessengerWeb do
 
       import CuriousMessengerWeb.ErrorHelpers
       import CuriousMessengerWeb.Gettext
+      import Phoenix.LiveView.Helpers
       alias CuriousMessengerWeb.Router.Helpers, as: Routes
     end
   end
@@ -50,6 +52,7 @@ defmodule CuriousMessengerWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
